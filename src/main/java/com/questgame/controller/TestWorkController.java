335f14ad796850e2af5fd41528c675781f6123c8
Zlata Migas <zlatamigas@gmail.com>
2021-05-13 09:17:02 +0300

    hello -> welcome

--- src/main/java/com/questgame/controller/TestWorkController.java
+++ src/main/java/com/questgame/controller/TestWorkController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestWorkController {
 
 
-    @GetMapping("/hello")
+    @GetMapping("/welcome")
     public String testHello(){
         return "Welcome! Controller works?";
     }
